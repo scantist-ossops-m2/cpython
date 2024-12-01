61092a77880bf8875607d5a66ab98cd3cc1d4baf
turly221 <feifan@scntist.com>
2024-12-01 20:55:34 +0000

    commit patch 11255496

--- Lib/ipaddress.py
+++ Lib/ipaddress.py
@@ -1406,7 +1406,7 @@ class IPv4Interface(IPv4Address):
             return False
 
     def __hash__(self):
-        return self._ip ^ self._prefixlen ^ int(self.network.network_address)
+        return hash((self._ip, self._prefixlen, int(self.network.network_address)))
 
     @property
     def ip(self):
@@ -2037,7 +2037,7 @@ class IPv6Interface(IPv6Address):
             return False
 
     def __hash__(self):
-        return self._ip ^ self._prefixlen ^ int(self.network.network_address)
+        return hash((self._ip, self._prefixlen, int(self.network.network_address)))
 
     @property
     def ip(self):
